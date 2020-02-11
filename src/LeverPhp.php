f251ece474d27b723a4d5c705785db5d12ca4790
Alfonso <strotgen@gmail.com>
2020-02-11 09:01:10 -0600

    Order of functions.
    Took 2 minutes

--- src/LeverPhp.php
+++ src/LeverPhp.php
@@ -69,68 +69,6 @@ class LeverPhp
         return $response;
     }
 
-
-    public function expand(string $expandable)
-    {
-        return $this->addParameter('expand', $expandable);
-    }
-
-    public function performAs(string $userId)
-    {
-        return $this->addParameter('perform_as', $userId);
-    }
-
-    public function include(string $includable)
-    {
-        return $this->addParameter('include', $includable);
-    }
-
-    /**
-     * @param string $field
-     * @param string|array $value
-     * @return $this
-     */
-    public function addParameter(string $field, $value)
-    {
-        if (!empty($field) && !empty($value)) {
-            $this->options['query'][$field] = is_array($value)
-                ? implode(',', $value)
-                : $value;
-        }
-
-        return $this;
-    }
-
-    public function leverKey(): string
-    {
-        return $this->leverKey;
-    }
-
-    public function client(): GuzzleClient
-    {
-        return $this->client;
-    }
-
-    public function offers()
-    {
-        // TODO next release.
-        // $regex = '/^opportunities\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/';
-        // if (preg_match($regex, $this->endpoint) === 0){
-        //     throw new Exception('Did not chain methods in correct order.');
-        // }
-
-        $this->endpoint .= '/offers';
-
-        return $this;
-    }
-
-    public function opportunities(string $opportunityId = '')
-    {
-        $this->endpoint = 'opportunities' . (empty($opportunityId) ? '' : '/' . $opportunityId) ;
-
-        return $this;
-    }
-
     public function create(array $body): array
     {
         $response = $this->responseToArray($this->post($body));
@@ -168,9 +106,70 @@ class LeverPhp
         });
     }
 
+    public function leverKey(): string
+    {
+        return $this->leverKey;
+    }
+
+    public function client(): GuzzleClient
+    {
+        return $this->client;
+    }
+
     private function responseToArray(ResponseInterface $response): array
     {
         return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function expand(string $expandable)
+    {
+        return $this->addParameter('expand', $expandable);
+    }
+
+    public function performAs(string $userId)
+    {
+        return $this->addParameter('perform_as', $userId);
+    }
+
+    public function include(string $includable)
+    {
+        return $this->addParameter('include', $includable);
+    }
+
+    /**
+     * @param string $field
+     * @param string|array $value
+     * @return $this
+     */
+    public function addParameter(string $field, $value)
+    {
+        if (!empty($field) && !empty($value)) {
+            $this->options['query'][$field] = is_array($value)
+                ? implode(',', $value)
+                : $value;
+        }
+
+        return $this;
+    }
+
+    public function opportunities(string $opportunityId = '')
+    {
+        $this->endpoint = 'opportunities' . (empty($opportunityId) ? '' : '/' . $opportunityId) ;
+
+        return $this;
+    }
+
+    public function offers()
+    {
+        // TODO next release.
+        // $regex = '/^opportunities\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/';
+        // if (preg_match($regex, $this->endpoint) === 0){
+        //     throw new Exception('Did not chain methods in correct order.');
+        // }
+
+        $this->endpoint .= '/offers';
+
+        return $this;
     }
 
 }
