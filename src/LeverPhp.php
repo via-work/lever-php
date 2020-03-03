c1497ecad524406740f1fc88266e4263fee7d7b8
omar sanchez <omarsanchezcas@gmail.com>
2020-03-03 14:27:41 -0600

    add stages to url

--- src/LeverPhp.php
+++ src/LeverPhp.php
@@ -247,6 +247,19 @@ class LeverPhp
         return $this;
     }
 
+    public function stages()
+    {
+        // TODO next release.
+        // $regex = '/^opportunities\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/';
+        // if (preg_match($regex, $this->endpoint) === 0){
+        //     throw new Exception('Did not chain methods in correct order.');
+        // }
+
+        $this->endpoint .= '/stage';
+
+        return $this;
+    }
+
     public function postings(string $postingId = '')
     {
         $this->endpoint = 'postings'.(empty($postingId) ? '' : '/'.$postingId);
